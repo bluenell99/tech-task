59c82ce185a41282a159b0cdc7e89d4f690863b3
JamieBlacknell <bluenell99@gmail.com>
2024-12-11 16:36:59 +0000

    Initial project drop

---
+++ Assets/Scenes/Diagnostic.cs
@@ -0,0 +1,20 @@
+﻿using TMPro;
+using UnityEngine;
+
+public abstract class Diagnostic : MonoBehaviour
+{
+
+    public string Label { get; private set; }
+    public float Measurement { get; private set; }
+
+    [SerializeField] private TextMeshProUGUI _label;
+    [SerializeField] private TextMeshProUGUI _measurement;
+    
+    public void SetDiagnostic(float measurement)
+    {
+        _measurement.text = measurement.ToString();
+    }
+
+    public abstract float GetDiagnostic();
+
+}
---
+++ Assets/Scenes/FramesPerSecondDiagnostic.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class FramesPerSecondDiagnostic : Diagnostic
+{
+    public override float GetDiagnostic()
+    {
+        float current = (int)(1f / Time.unscaledDeltaTime);
+        return current;
+    }
+}
